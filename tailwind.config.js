352afdfb714a8b35487e0dddcc54a7a41d8a9f25
piccassol <noahmichel999@gmail.com>
2025-12-14 18:02:58 -0500

    updating tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss"
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -53,5 +52,3 @@ const config: Config = {
   },
   plugins: [require("tailwindcss-animate")],
 }
-
-export default config
